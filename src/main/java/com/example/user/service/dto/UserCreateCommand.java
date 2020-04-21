22d57d205cc2b17b3ebe2f3a0010188fe4c5df89
baka-lan <evg.bakl@gmail.com>
2020-04-21 09:54:01 +0700

    Updated UserCreateCommand

--- src/main/java/com/example/user/service/dto/UserCreateCommand.java
+++ src/main/java/com/example/user/service/dto/UserCreateCommand.java
@@ -2,7 +2,6 @@ package com.example.user.service.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -10,6 +9,15 @@ import javax.validation.constraints.Size;
 public class UserCreateCommand {
     @NotBlank(message = "Login cannot be empty")
     private String login;
+
+    @NotBlank(message = "Firstname cannot be empty")
+    private String firstname;
+
+    @NotBlank(message = "Middlename cannot be empty")
+    private String middlename;
+
+    @NotBlank(message = "Lastname cannot be empty")
+    private String lastname;
 
     @Size(min = 3, message = "Password should be greater than 3 symbols")
     private String password;
